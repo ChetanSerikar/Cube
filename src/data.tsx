cbd0baa523e62485b9a3d79c742838c010edbf38
ChetanSerikar <chetanserikar87@gmail.com>
2024-08-11 13:55:06 +0530

    first commit

--- src/data.tsx
+++ src/data.tsx
@@ -1,4 +1,4 @@
-export let customers = new Array(1000).fill({}).map((item, i) => ({
+export const customers = new Array(1000).fill({}).map((item, i) => ({
   id: i,
   name: `Customer ${i}`,
   title: `Title ${i} Lorem ipsum dolor, si amet consectetur adipisicing elit. Esse amet fugiat modi voluptate inventore cum doloremque optio repellat adipisci? Voluptate ipsam enim optio possimus et fugit animi suscipit, voluptatibus praesentium!
